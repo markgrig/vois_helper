d67b46d4ef620b80bc1f395de238a7d3e5df37a6
markgrig <110450201+markgrig@users.noreply.github.com>
2022-08-24 11:28:05 +0300

    optimization 24.08.2022

--- Talk.py
+++ Talk.py
@@ -1,8 +1,7 @@
+from copyreg import constructor
 import apiai, json, re
 import pyttsx3
-
 import speech_recognition as sr
-
 import webbrowser
 import subprocess
 import time
@@ -10,10 +9,12 @@ import psutil
 import pyautogui as pag
 import eel
 from threading import Thread
-
 import os
 import random
+import pygame
 
+#функция для удаление файлов, в дальнейшем планируется 
+#использовать подход записи файлов для голосовых сообщений с телефона
 @eel.expose
 def delete_file(file):
     if os.path.isfile(file):
@@ -21,163 +22,130 @@ def delete_file(file):
         print("success")
     else: print("File doesn't exists!")
 
-
-import pygame
+#добавление проигроваемых звуков, проигрывание вступления 
 pygame.init()
-song = pygame.mixer.Sound('output/loading.mp3')
-song1 = pygame.mixer.Sound('output/appearance.mp3')
-song2 = pygame.mixer.Sound('output/disappearance.mp3')
-song3 = pygame.mixer.Sound('output/bell.mp3')
+songLodaing = pygame.mixer.Sound('output/loading.mp3')
+songExecutes  = pygame.mixer.Sound('output/executes.mp3')
+songDisappearance = pygame.mixer.Sound('output/disappearance.mp3')
+songAppearance = pygame.mixer.Sound('output/appearance.mp3')
 pygame.mixer.music.set_volume(2)
 clock = pygame.time.Clock()
-song.play()
+songLodaing.play()
 
+#инициализация графики 
+eel.init("web")
+
+#Инициализация синтезатора голоса  
 tts = pyttsx3.init()
 rate = tts.getProperty('rate')
 tts.setProperty('rate', rate-40)
 volume = tts.getProperty('volume')
 tts.setProperty('volume', volume+0.9)
 voices = tts.getProperty('voices')
-x_v = len(voices)
+NumberOfHelper = len(voices)
+
+#Поиск голосового пакета Татьяна
 i = 0
-while i < x_v:
+while i < NumberOfHelper:
     if 'Tatyana RSI' in voices[i].id:
         print(voices[i].id)
         tts.setProperty('voice', voices[i].id)
     i += 1
 
-
-start = "Привет! Меня зовут Лили'т! Чем я' могу' , вам  помо'чь? "
-#tts.say( "Hello!" )
-tts.say( start )
+#Приветствие голосового помошника
+assistantGreeting = "Привет! Меня зовут Лили'т! Чем я' могу' , вам  помо'чь? "
+tts.say( assistantGreeting )
 tts.runAndWait()
 
-back = { 'пока', 'уйди', 'спасибо ты не нужна', 'скройся', 'исчезни', 'ты не нужна', 'давай потом', 'затихни', 'тихо', 'жди', 'ожидай','режим ожидания', 'ожидание' ,
-'протокол ждун', 'режим ждун', 'запустить режим ждун', 'режим жди', 'ждун', 'стать ждуном', 'будь ждуном', 'ты ждун', 'обожди', 'жди меня', 'помолчи','испарись'
-'протокол ожидание', 'запустить протокол ожидания', 'запустить режим ожидания','всё' }
-comeback = {'лилит', 'лилия', 'лиля', 'ли ли', 'лил', 'лилии', 'лилию', 'вернись', 'ли вернись', 'ли ты где', 'бот', 'робот', 'робот', 'комп', 'компьютер'}
+#Фразы, чтобы позвать помошника, или "отпустить"
+releaseAssistant  = ['лилит', 'лилия', 'лиля', 'ли ли', 'лил', 'лилии', 'лилию', 'вернись',
+ 'ли вернись', 'ли ты где', 'бот', 'робот', 'робот', 'комп', 'компьютер' ]
+callAssistant = [ 'пока', 'уйди', 'спасибо ты не нужна', 'скройся', 'исчезни', 'ты не нужна', 
+'давай потом', 'затихни', 'тихо', 'жди', 'ожидай','режим ожидания', 'ожидание' ,
+'протокол ждун', 'режим ждун', 'запустить режим ждун', 'режим жди', 'ждун', 'стать ждуном',
+'будь ждуном', 'ты ждун', 'обожди', 'жди меня', 'помолчи','испарись'
+'протокол ожидание', 'запустить протокол ожидания', 'запустить режим ожидания','всё' ]
 
-startrememb="0"
-rememb2 = startrememb
-rememb = startrememb
+startlisteningMode="0"
 
-eel.init("web")
-
-def talk(text):
+#функция говорения помощника через питон, звук воспроизводится на компьюторе
+#не подходит для решений, где хотелось бы слышать ответы через телефон
+def assistantSays(text):
     tts.say( text )
     tts.runAndWait()
 
-def querlfun(text):
+#Класс для поиска решений Ассистентом 
+class findSolution():
 
-    Querval = 0
+    def __init__(self, text): 
+        self.__querval = text 
+    
+    def findAnswer(self):
 
-    if 'открой' in text:
-        if 'vk' or "вк" in text:
-            Querval = 'https://vk.com/feed'
-    if 'открой сообщения'in text:
-        Querval =  'https://vk.com/im?v='
-    if ( 'открой' or 'открой ютуб' ) in text:
-         if 'youtube' or "ютуб" in text:
-             Querval =  'https://www.youtube.com/'
-    if 'открой сберзвук'in text:
-        Querval =  'https://sber-zvuk.com/'
-    if 'открой мой сайт' in text:
-         Querval = 'http://localhost/scientist.php'
-    if 'запусти сервер' in text:
-        Querval =  'D:\Program\OpenServer\Open Server.exe'
-    if 'запусти чат' in text:
-         Querval =  'chat.js' ,
-    if 'открой атом' in text:
-         Querval =  'D:\Program\atom-1-60\Atom\atom.exe'
-    if 'открой bandicam' in text:
-        Querval =  'D:\Program\Bandicam\loader.exe'
-    if 'открой геншин' in text:
-        Querval =  'C:\Program Files\Genshin Impact\launcher.exe'
-    if 'открой плейлисты сберзвук' in text:
-        Querval = 'https://sber-zvuk.com/playlists'
-    if 'открой категории сберзвук' in text:
-        Querval =  'https://sber-zvuk.com/genres'
+        dirQuervals =  {
+        'https://vk.com/feed': [ 'открой вк' , 'открой vk' ] ,
+        'https://vk.com/im?v=' : [ 'открой сообщения' ]  ,
+        'https://www.youtube.com/' : [ 'открой ютуб' ,'открой youtube'   ] ,
+        'C:\Program Files (x86)\Bandicam\Loader.exe' : [ 'открой bandicam' , 'запись экрана'] ,
+        'https://sber-zvuk.com/playlists' : [ 'не знаю что послушать'  ] ,
+        'https://sber-zvuk.com/genres' : [ 'открой категории' ] ,
+        'https://www.google.com/search?q=' : [ 'фильм', 'сериал' , 'аниме' ] ,
+        'https://sber-zvuk.com/search?query=' : [ 'найди музыку' ] ,
+        'https://www.google.com/search?q=' : [  'найти на google' , 'найти на гугл'  ] ,
+        'https://www.youtube.com/results?search_query=' : [  'найти на youtube' , 'найти на ютуб'] ,
+        'https://sber-zvuk.com/genre/hiphop':  [  "хочу хип-хоп" ,  "хочу хипхоп" , "хочу хип хоп" ] ,
+        'https://sber-zvuk.com/genre/relax' : [  "хочу отдых" ] ,
+        'https://sber-zvuk.com/genre/party' : [  "хочу вечеринку"] ,
+        'https://sber-zvuk.com/genre/pop' : [  "хочу попсу"] ,
+        'https://sber-zvuk.com/genre/sport' : [  "хочу тренировку", "хочу зарядку", "хочу спорт"] ,
+        'https://sber-zvuk.com/genre/rock' : [ "хочу рок" , "рок" , "хочу rock" , "rock" ] ,
+        'https://sber-zvuk.com/genre/motivation' : [ 'хочу мотивацию' , "мотивацию" , "мотивация" ] ,
+        'https://sber-zvuk.com/genre/melancholy' : [  'хочу меланхолию' , "хочу погрустить" ] ,
+        'https://sber-zvuk.com/genre/travel' : [  'хочу путешестовать' , 'хочу в путешествие' ] 
+        }
 
-    if 'хочу фильм' in text:
-        text = text.replace('хочу','')
-        text = text + ' смотреть онлайн бесплатно в хорошем качестве'
-        text =text.replace(' ','+')
-        Querval = 'https://www.google.com/search?q=' + text
+        for answer, quervals in dirQuervals.items():
+            if self.__querval  in quervals:
+                return answer
+        return ""
+    
+    def helpUser( self, Querval , text ): 
+        print(Querval)
+        solution = ""
+        dirSolution = {
+            'disk' : [ 'D:' ,  'C:' ] ,
+            'url' : 'http' ,
+        } 
+        
+        for answer, solutions in dirSolution.items():
+            for i in range(len(solutions)):
+                if solutions[i] in Querval:
+                    solution = answer
+        
+        if ( solution=='url' ): 
+            songAppearance.play()
+            songExecutes.play()
+            webbrowser.open_new(Querval)
 
-    if 'хочу сериал' in text:
-        text = text.replace('хочу ','')
-        text = text + ' смотреть онлайн бесплатно в хорошем качестве'
-        text =text.replace(' ','+')
-        Querval = 'https://www.google.com/search?q=' + text
-
-    if 'хочу аниме' in text:
-        text = text.replace('хочу ','')
-        text = text + ' смотреть онлайн бесплатно в хорошем качестве'
-        text =text.replace(' ','+')
-        Querval = 'https://www.google.com/search?q=' + text
-
-    if 'найди на' in text:
-
-        if 'сберзвук' in text:
-            text = text.replace('найди на сберзвук','')
-            text =text.replace(' ','+')
-            Querval = 'https://sber-zvuk.com/search?query=' + text
-        if 'youtube'in text or 'ютуб '  in text:
-            text = text.replace('найди на youtube','')
-            text = text.replace('найди на youtube','')
-            text =text.replace(' ','+')
-            Querval = 'https://www.youtube.com/results?search_query=' + text
-        else:
-            if 'google' in text or 'гугл' in text:
-                text = text.replace('найди на ','')
-                text = text.replace('google','')
-                text = text.replace('гугл','')
-                text =text.replace(' ','+')
-                Querval = 'https://www.google.com/search?q=' + text
-
-    if 'хочу хип-хоп' in text:
-        Querval = 'https://sber-zvuk.com/genre/hiphop'
-    if 'хочу relax' in text or 'хочу релакс' in text:
-        Querval = 'https://sber-zvuk.com/genre/relax'
-    if 'хочу party' in text or 'хочу пати' in text:
-        Querval = 'https://sber-zvuk.com/genre/party'
-    if 'хочу попсу' in text:
-        Querval = 'https://sber-zvuk.com/genre/pop'
-    if 'тренировка' in text:
-        Querval = 'https://sber-zvuk.com/genre/sport'
-    if 'хочу rock' in text or 'хочу рок' in text:
-        Querval = 'https://sber-zvuk.com/genre/rock'
-    if 'хочу мотивацию' in text:
-        Querval = 'https://sber-zvuk.com/genre/motivation'
-    if 'хочу погрустить' in text:
-        Querval = 'https://sber-zvuk.com/genre/melancholy'
-    if 'хочу в дорогу' in text:
-        Querval = 'https://sber-zvuk.com/genre/travel'
-    if 'хочу танцевать' in text:
-        Querval = 'https://sber-zvuk.com/playlist/6571612'
-    if 'хочу в клуб' in text:
-        Querval = 'https://sber-zvuk.com/playlist/6571191'
-    if 'хочу латинских чик' in  text:
-        Querval = 'https://sber-zvuk.com/playlist/7021296'
-    if 'хочу хаус' in text:
-        Querval = 'https://sber-zvuk.com/playlist/7021572'
-    if 'хочу отдохнуть' in text:
-        Querval = 'https://sber-zvuk.com/playlist/6993920'
-
-    if 'хочу перезагрузку' in text:
-        Querval = 'https://sber-zvuk.com/playlist/6856417'
-    if 'хочу акустику' in text:
-        Querval = 'https://sber-zvuk.com/playlist/6346741'
-    if 'хочу дозу' in text:
-        Querval = 'https://sber-zvuk.com/playlist/7147193'
+        if 'хочу' in text:
+            time.sleep(6)
+            pag.click()
+            time.sleep(2)
+            pag.press('space')
+            time.sleep(2)
+            tts.say('Надеюсь вам понравиться!')
+            eel.call_in_py('Надеюсь вам понравиться!')
+            tts.runAndWait()
 
 
-    return Querval
+        if ( solution =='disk'):
+            songAppearance.play()
+            songExecutes.play()
+            subprocess.Popen(Querval)
 
 
 
-def record_volume(rememb):
-    #print(rememb)
+def record_volume(listeningMode):
     r = sr.Recognizer()
     with sr.Microphone(device_index = 1) as source:
         print('Настраиваюсь.')
@@ -190,7 +158,7 @@ def record_volume(rememb):
         text = query.lower()
 
 
-        if text in back:
+        if text in callAssistant:
             print('off')
             num_rand =  random.randint(-1000000, 1000000)
 
@@ -199,9 +167,9 @@ def record_volume(rememb):
             #tts.say('Хорошо...')
             tts.save_to_file('Хорошо...', "./web/audio/answer" + str(num_rand) + ".mp3" )
             tts.runAndWait()
-            rememb="off";
+            listeningMode="off";
 
-        if  text in comeback:
+        if  text in releaseAssistant:
             
             print('on')
             num_rand =  random.randint(-1000000, 1000000)
@@ -210,78 +178,32 @@ def record_volume(rememb):
             #tts.say('Да, господин! Вам.. Что-то нужно? ')
             tts.save_to_file('Да, господин! Что нужно?', "./web/audio/answer" + str(num_rand)  + ".mp3" )
             tts.runAndWait()
-            rememb="on";
+            listeningMode="on";
 
-        if (rememb == 'on'):
-            if  text in comeback:
+        if (listeningMode == 'on'):
+            if  text in releaseAssistant:
                 print('Вы позвали лилит')
             else:
                 print(f'Вы сказали: {query.lower()}')
                 eel.call_in_py3(text)
-            #talk(text)
-
-        Querval = querlfun(text)
-        print(Querval)
-        res=0
-
-        if Querval!=0 and ('D:' in Querval or 'C:' in Querval ):
-            res='disk'
-
-        if Querval!=0 and 'http' in Querval :
-            res='url'
-
-        if Querval!=0 and 'chat.js' in Querval  :
-            res='node'
-        print(res)
-        if ( Querval!=0 and res=='url'):
-            song1.play()
-            song3.play()
-            webbrowser.open_new(Querval)
-
-            pag.press('space')
-            if 'хочу' in text:
-                eel.call_in_py('Надеюсь вам понравиться!')
-                time.sleep(6)
-                pag.click()
-                time.sleep(2)
-                pag.press('space')
-                time.sleep(2)
-
-                num_rand =  random.randint(-1000000, 1000000)
-
-                tts.save_to_file('Надеюсь вам понравиться!', "./web/audio/answer" + str(num_rand)  + ".mp3" )
-                tts.runAndWait()
-                rememb="off";
-
-        if ( Querval!=0 and res=='disk'):
-            song1.play()
-            song3.play()
-            subprocess.Popen(Querval)
-
-
-        if ( Querval!=0 and res=='node'):
-            song1.play()
-            song3.play()
-            x = subprocess.Popen(['start', 'powershell'], shell = True)
-            x.wait()
-            time.sleep(2) #чтобы компьютер успел открыть консоль
-            pag.typewrite('node C:\prog\openserver\domains\localhost')
-
-            pag.press('enter')
-
+            
+        objectSolution = findSolution(text)
+        Querval = objectSolution.findAnswer()
+        objectSolution.helpUser(Querval, text)
+        
     except:
-        if rememb == 'on' and rememb2 == 'on' :
+        if listeningMode == 'on' and startlisteningMode == 'on' : 
             tts.say('Ой, я не поняла.')
             eel.call_in_py('Ой, я не поняла.')
             tts.runAndWait()
 
-    #print(rememb)
-    newrememb= rememb
-    return newrememb
+    #print(listeningMode)
+    newlisteningMode= listeningMode
+    return newlisteningMode
 
 @eel.expose
 def text_talk(text):
-        if text in back:
+        if text in callAssistant:
             print('off')
             #tts.say('Хорошо...')
             num_rand =  random.randint(-1000000, 1000000)
@@ -291,7 +213,7 @@ def text_talk(text):
             tts.runAndWait()
 
 
-        if  text in comeback:
+        if  text in releaseAssistant:
             print('on')
            
             num_rand =  random.randint(-1000000, 1000000)
@@ -301,51 +223,10 @@ def text_talk(text):
             #tts.say('Да, господин! Вам.. Что-то нужно?')
             tts.runAndWait()
 
-        Querval = querlfun(text)
-        print(Querval)
-        res=0
-
-        if Querval!=0 and ('D:' in Querval or 'C:' in Querval ):
-            res='disk'
-
-        if Querval!=0 and 'http' in Querval :
-            res='url'
-
-        if Querval!=0 and 'chat.js' in Querval  :
-            res='node'
-            print(res)
-        if ( Querval!=0 and res=='url'):
-            song1.play()
-            song3.play()
-            webbrowser.open_new(Querval)
-
-            pag.press('space')
-            if 'хочу' in text:
-                time.sleep(6)
-                pag.click()
-                time.sleep(2)
-                pag.press('space')
-                time.sleep(2)
-                tts.say('Надеюсь вам понравиться!')
-                eel.call_in_py('Надеюсь вам понравиться!')
-                tts.runAndWait()
-
-
-        if ( Querval!=0 and res=='disk'):
-            song1.play()
-            song3.play()
-            subprocess.Popen(Querval)
-
-
-        if ( Querval!=0 and res=='node'):
-            song1.play()
-            song3.play()
-            x = subprocess.Popen(['start', 'powershell'], shell = True)
-            x.wait()
-            time.sleep(2) #чтобы компьютер успел открыть консоль
-            pag.typewrite('node d:\Program\OpenServer\domains\localhost')
-
-            pag.press('enter')
+        objectSolution = findSolution(text)
+        Querval = objectSolution.findAnswer()
+        objectSolution.helpUser(Querval, text)
+        
         return 0
 
 @eel.expose
@@ -358,22 +239,20 @@ def call_in_Li(x):
 
 @eel.expose
 def test_on_off(x):
-    rememb2 = x
-    if rememb2=="on":
-        song3.play()
-    if rememb2=="off":
-        song2.play()
+    startlisteningMode = x
+    if startlisteningMode=="on":
+        songAppearance.play()
+    if startlisteningMode=="off":
+        songDisappearance.play()
     print(x)
-    return rememb2
+    return startlisteningMode
 
 def my_other_thread1():
-    rememb="on"
+    listeningMode="on"
     while True:
-        rememb2 = eel.call_in_py2()()
-        if rememb2=="on":
-            rememb = record_volume(rememb)
-
-
+        startlisteningMode = eel.call_in_py2()()
+        if startlisteningMode=="on":
+            listeningMode = record_volume(listeningMode)
 
 
 #eel.call_in_py("Test Py")
